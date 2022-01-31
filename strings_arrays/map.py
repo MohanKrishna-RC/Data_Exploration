2351354d0939b67515def18c89f29a954c80284c
MOHANKRISHNA LANDA <mohan.landa@inference.in>
2022-01-31 14:55:01 +0530

    "updated"

--- strings_arrays/map.py
+++ strings_arrays/map.py
@@ -164,3 +164,16 @@ def fun(s):
 
 def filter_mail(emails):
     return list(filter(fun, emails))
+
+
+import numpy as np
+def find_nearest_value(arr, value):
+    # arr = np.asarray(arr)
+    print(arr)
+    idx = (np.abs(arr - value)).argmin()
+    print(idx)
+    return arr[idx]
+#Driver code
+arr = np.array([ 0.21169,  0.61391, 0.6341, 0.0131, 0.16541,  0.5645,  0.5742])
+value = 0.52
+print(find_nearest_value(arr, value))
