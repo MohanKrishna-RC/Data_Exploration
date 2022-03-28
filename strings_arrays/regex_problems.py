364af278e77bd3cef27bbd634c50a6a9571ebfd5
MOHANKRISHNA LANDA <mohan.landa@inference.in>
2022-03-28 16:51:04 +0530

    Updated

--- strings_arrays/regex_problems.py
+++ strings_arrays/regex_problems.py
@@ -55,3 +55,49 @@ for match in re.finditer(pattern, text):
     e = match.end()
     print('Found "%s" at %d:%d' % (text[s:e], s, e))
 """
+
+#find the string of length 10>5, min 5, <5
+text = 'Python exercises, PHP exercises, C# exercises'
+
+sent_min5 = re.findall(r'[a-zA-Z]{5,}',text)
+sent_max5 = re.findall(r'[a-zA-Z]{1,5}',text)
+sent_limit = re.findall(r'[a-zA-Z]{5,10}',text)
+print(sent_min5,sent_max5,sent_limit)
+
+#convert camel-case(PythonExercises) to snake case(python_exercises)
+
+camel_text = "PythonExercisesAreGood"
+def camel_to_snake(text):
+     str1 = re.sub('(.)([A-Z][a-z]+)',r'\1_\2',text)
+     print(str1)
+     return re.sub('([a-z0-9])([A-Z])',r'\1_\2',str1).lower()
+
+print(camel_to_snake(camel_text))
+
+#Covert snake case string to camel case string
+snake_text = "python_exercises_are_good"
+def snake_to_camel(text):
+     return "".join(x.capitalize() or '_' for x in snake_text.split('_'))
+print(snake_to_camel(snake_text))
+
+#find text between quotation marks ("")
+text = '"Python","JAVA","Lovaf"'
+print(re.findall(r'"(.*?)"',text))
+
+#remove mutlispace in a string and whitespaces
+text = "Python   Exercises   are   good"
+print(re.sub(r' +','_',text))
+print(re.sub(r' +',' ',text))
+print(re.sub(r' +','',text)) #remove whitespaces
+print(re.sub(r'\s+','',text)) #remove whitespaces
+
+#remove all characters except alphanumeric characters
+
+text1 = '**//Python Exercises// - 12. '
+pattern = re.compile('[\W_]+')
+print(pattern.sub('',text1))
+print(re.sub(r'[\W_]+','',text1))
+
+#split string at uppercase letters
+text = "Python Exercises Are Good"
+print(re.findall(r'[A-Z][^A-Z]*',text))
